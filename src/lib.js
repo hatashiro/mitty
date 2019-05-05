46c9d48a83ecc01e4333813b42f84e82459fc52b
Hyunje Jun <me@noraesae.net>
2019-05-05 18:39:04 +0900

    Add lib

---
+++ src/lib.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+if (process.stdin.setRawMode) {
+  process.stdin.setRawMode(true);
+}
+
+const f = fs.openSync('/dev/stdin', 0);
+
+export function getChar() {
+  const b = Buffer.alloc(1);
+  while (true) {
+    try {
+      fs.readSync(f, b, 0, 1, null);
+      return b[0];
+    } catch (err) {
+      continue;
+    }
+  }
+}
+
+export function putChar(c) {
+  process.stdout.write(String.fromCharCode(c));
+}
