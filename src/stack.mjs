d7cbb71604f22e01667b0acd895dd3a18f1636b1
Hyunje Jun <me@noraesae.net>
2019-05-05 23:12:49 +0900

    Add stack implementation

---
+++ src/stack.mjs
@@ -0,0 +1,19 @@
+export class Stack {
+  arr = [];
+
+  get size() {
+    return this.arr.length;
+  }
+
+  get top() {
+    return this.arr[this.arr.length - 1];
+  }
+
+  push(el) {
+    this.arr.push(el);
+  }
+
+  pop() {
+    return this.arr.pop();
+  }
+}
