7e6c98b82cc15dde74cbb4f58951c7a0162c2b01
Hyunje Jun <me@noraesae.net>
2019-05-07 03:44:37 +0900

    Implement compiler

--- src/compiler.mjs
+++ src/compiler.mjs
@@ -1,40 +1,145 @@
+import { beautify } from "s-exify/module";
 import { parse } from "./parser";
 import { optimize } from "./optimizer";
 import { wat2wasm } from "./wabt";
 
 class Compiler {
-  wat = "";
+  generate = node => this[node.type](node.data);
 
-  generate = node => {
-    this[node.type](node.data);
-  };
+  Program(nodes) {
+    return `
+      (module
+        (import "lib" "getChar" (func $getChar (result i32)))
+        (import "lib" "putChar" (func $putChar (param i32)))
 
-  get wasm() {
-    return wat2wasm(this.wat);
+        (memory 2048)
+
+        (global $pointer (mut i32) (i32.const 0))
+
+        (func $Pointer (param $diff i32)
+          (global.get $pointer)
+          (local.get $diff)
+          (i32.const 4)
+          (i32.mul)
+          (i32.add)
+          (global.set $pointer)
+          )
+
+        (func $Value (param $diff i32)
+          (global.get $pointer)
+          (global.get $pointer)
+          (i32.load)
+          (local.get $diff)
+          (i32.add)
+          (i32.store)
+          )
+
+        (func $Zero
+          (global.get $pointer)
+          (i32.const 0)
+          (i32.store)
+          )
+
+        (func $Mul (param $pointerDiff i32) (param $valueDiff i32) (local $targetPointer i32)
+          (global.get $pointer)
+          (local.get $pointerDiff)
+          (i32.const 4)
+          (i32.mul)
+          (i32.add)
+          (local.set $targetPointer)
+
+          (local.get $targetPointer)
+
+          (local.get $targetPointer)
+          (i32.load)
+
+          (global.get $pointer)
+          (i32.load)
+          (local.get $valueDiff)
+          (i32.mul)
+
+          (i32.add)
+
+          (i32.store)
+          )
+
+        (func $main
+          ${
+            nodes.map(this.generate).join("\n")
+          }
+          )
+
+        (start $main)
+      )
+    `;
   }
 
-  Program(nodes) {}
+  Pointer(diff) {
+    return `
+      (i32.const ${diff})
+      (call $Pointer)
+    `;
+  }
 
-  Pointer(diff) {}
+  Value(diff) {
+    return `
+      (i32.const ${diff})
+      (call $Value)
+    `;
+  }
 
-  Value(diff) {}
+  Zero() {
+    return `
+      (call $Zero)
+    `;
+  }
 
-  Zero() {}
+  Mul({ pointerDiff, valueDiff }) {
+    return `
+      (i32.const ${pointerDiff})
+      (i32.const ${valueDiff})
+      (call $Mul)
+    `;
+  }
 
-  Mul({ pointerDiff, valueDiff }) {}
+  GetChar() {
+    return `
+      (call $getChar)
+    `;
+  }
 
-  GetChar() {}
+  PutChar() {
+    return `
+      (global.get $pointer)
+      (i32.load)
+      (call $putChar)
+    `;
+  }
 
-  PutChar() {}
-
-  Loop(nodes) {}
+  Loop(nodes) {
+    return `
+      (loop
+        (global.get $pointer)
+        (i32.load)
+        (if
+          (then
+            ${
+              nodes.map(this.generate).join("\n")
+            }
+            (br 1)
+            )
+          )
+        )
+    `;
+  }
 }
 
 export function compile(code) {
   const program = optimize(parse(code));
   const compiler = new Compiler();
-  compiler.generate(program);
 
-  const { wat, wasm } = compiler;
+  const wat = beautify(compiler.generate(program));
+  const wasm = wat2wasm(wat);
+
   return { wat, wasm };
 }
