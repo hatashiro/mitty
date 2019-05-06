6a0dd7471635b2c6876f778bac0a70131a03d20d
Hyunje Jun <me@noraesae.net>
2019-05-06 18:01:24 +0900

    Format wabt.mjs

--- src/wabt.mjs
+++ src/wabt.mjs
@@ -1,17 +1,17 @@
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 import { fileSync } from "tmp";
-import { resolve } from 'path';
-import { writeFileSync, readFileSync } from 'fs';
+import { resolve } from "path";
+import { writeFileSync, readFileSync } from "fs";
 
 const tempFile = ext => fileSync({ mode: 0o644, postfix: `.${ext}` }).name;
 
 const bin = {
-  wat2wasm: resolve(new URL(import.meta.url).pathname, '../wabt/bin/wat2wasm')
+  wat2wasm: resolve(new URL(import.meta.url).pathname, "../wabt/bin/wat2wasm")
 };
 
 export function wat2wasm(watStr) {
-  const watFile = tempFile('wat');
-  const wasmFile = tempFile('wasm');
+  const watFile = tempFile("wat");
+  const wasmFile = tempFile("wasm");
   writeFileSync(watFile, watStr);
   execSync(`${bin.wat2wasm} ${watFile} -o ${wasmFile}`);
   return readFileSync(wasmFile);
